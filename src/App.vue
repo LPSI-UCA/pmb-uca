54b866babe92803789b10b7fe0e63b886a1d5cba
azkifairuz <azkiajmal@gmail.com>
2023-11-29 16:17:16 +0700

    install tailwindcss and vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="flex justify-center h-screen items-center">
+    <h1 class="text-3xl font-bold">Halaman utama</h1>
+  </div>
+  
+</template>
+
+
